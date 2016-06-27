0fbafda310a9a6f14346e84b185ed4077928897c
Haziman Hashim <haziman@abh.my>
2016-06-28 05:33:53 +0800

    compatibility patch

--- lib/lita/adapters/telegram.rb
+++ lib/lita/adapters/telegram.rb
@@ -11,6 +11,7 @@ module Lita
       end
 
       def run
+				
         client.listen do |message|
           user = Lita::User.find_by_name(message.from.username)
           user = Lita::User.create(message.from.username) unless user
@@ -25,12 +26,17 @@ module Lita
           robot.receive(msg)
         end
       end
+			
       def send_messages(target, messages)
         messages.each do |message|
-          client.api.sendChatAction(chat_id: target.room.to_i, action: 'typing')
+        	#client.api.sendChatAction(chat_id: target.room.to_i, action: 'typing')
           client.api.sendMessage(chat_id: target.room.to_i, text: message)
         end
       end
+			
+			def shutdown
+				Lita.logger.info "Shutting Down..."
+			end
 
       Lita.register_adapter(:telegram, self)
     end
