5bd1070aae32dc6d99c92659252541b364e1538f
Haziman Hashim <haziman@abh.my>
2016-08-08 20:53:24 +0800

    webhook

--- lib/lita/adapters/telegram.rb
+++ lib/lita/adapters/telegram.rb
@@ -10,47 +10,51 @@ module Lita
 			end
 
 			def run
-				client.logger.info "Webhook enabled"
-				client.listen do |message|
-					user = Lita::User.find_by_name(message.from.username)
-					user = Lita::User.create(message.from.id, {
-						name: message.from.username,
-						mention_name: "@#{message.from.username}",
-					}) unless user
+				if defined?(Lita::Handlers::Webhook)
+					client.logger.info "Webhook enabled"
+					sleep
+				else
+					client.listen do |message|
+						user = Lita::User.find_by_name(message.from.username)
+						user = Lita::User.create(message.from.id, {
+							name: message.from.username,
+							mention_name: "@#{message.from.username}",
+						}) unless user
 
-					if message.class.name == 'Telegram::Bot::Types::Message'
-						chat = Lita::Room.new(message.chat.id)
-						bot_query = message.text || ''
-					elsif message.class.name == 'Telegram::Bot::Types::InlineQuery'
-						chat = Lita::Room.new(-1)
-						bot_query = "inline #{message.query}"
-					else
-						bot_query = ""
-					end
-
-					unless bot_query.empty?
-
-						if bot_query[0].match('/')
-							matches, command, botname, args = bot_query.match(/\/?([^\@\s]+)(\@[^\s]+)?\s*(.+)?/).to_a
-							if command.match(/start|startgroup/) and !args.nil?
-								args = args.split(' ')
-								command = args.shift
-								args = args.join(' ')
-							end
+						if message.class.name == 'Telegram::Bot::Types::Message'
+							chat = Lita::Room.new(message.chat.id)
+							bot_query = message.text || ''
+						elsif message.class.name == 'Telegram::Bot::Types::InlineQuery'
+							chat = Lita::Room.new(-1)
+							bot_query = "inline #{message.query}"
 						else
-							matches, botname, command, args = bot_query.match(/(#{robot.mention_name})?\s*([^\s]+)\s*(.+)?/).to_a
+							bot_query = ""
 						end
-						botname ||= robot.mention_name
 
-						client.logger.info("botname: #{botname}, command: #{command}, args: #{args}")
-						next if !botname.match(robot.mention_name)
+						unless bot_query.empty?
 
-						bot_query = [botname, command, args].join(' ')
+							if bot_query[0].match('/')
+								matches, command, botname, args = bot_query.match(/\/?([^\@\s]+)(\@[^\s]+)?\s*(.+)?/).to_a
+								if command.match(/start|startgroup/) and !args.nil?
+									args = args.split(' ')
+									command = args.shift
+									args = args.join(' ')
+								end
+							else
+								matches, botname, command, args = bot_query.match(/(#{robot.mention_name})?\s*([^\s]+)\s*(.+)?/).to_a
+							end
+							botname ||= robot.mention_name
 
-						source = Lita::Source.new(user: user, room: chat)
-						msg = Lita::Message.new(robot, bot_query.strip, source)
-						msg.raw = message
-						robot.receive(msg)
+							client.logger.info("botname: #{botname}, command: #{command}, args: #{args}")
+							next if !botname.match(robot.mention_name)
+
+							bot_query = [botname, command, args].join(' ')
+
+							source = Lita::Source.new(user: user, room: chat)
+							msg = Lita::Message.new(robot, bot_query.strip, source)
+							msg.raw = message
+							robot.receive(msg)
+						end
 					end
 				end
 			end
